030d8e405a8d99f308d8e70954393e4ff48bd241
Sabeel Khan <sabeelhps@gmail.com>
2025-10-17 21:49:22 +0530

    More into events

---
+++ Javascript/Class-7/MakeTheFilterWork/script.js
@@ -0,0 +1,13 @@
+/*
+
+    Problem-1
+    Problem Statement
+
+    Develop a dynamic filter functionality for a webpage that lists movies 
+    by their categories, such as Action, Romance, and Comedy. 
+    When a user selects a category from a dropdown menu, 
+    the page should update to display only the movies that belong to the selected 
+    category. If "None" is selected, all movies should be shown. 
+    This feature should enhance the user experience by allowing 
+    them to easily find movies of their interest without scrolling through all available options.
+*/
