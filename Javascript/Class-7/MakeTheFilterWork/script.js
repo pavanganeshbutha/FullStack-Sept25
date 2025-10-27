4da1247eed0aa1afb36ca48305b7bac8078fd6c0
Sabeel Khan <sabeelhps@gmail.com>
2025-10-27 23:27:28 +0530

    AJAX

--- Javascript/Class-7/MakeTheFilterWork/script.js
+++ Javascript/Class-7/MakeTheFilterWork/script.js
@@ -11,3 +11,31 @@
     This feature should enhance the user experience by allowing 
     them to easily find movies of their interest without scrolling through all available options.
 */
+
+const select = document.querySelector('select');
+const divs = document.querySelectorAll('.movies')
+
+function filterAndDisplay(category) {
+    if (category === 'none') {
+        for (let div of divs) {
+            div.style.display = 'block';
+        }
+    } else {
+        for (let div of divs) {
+            const currCategory = div.children[1].getAttribute('data-category');
+            if (currCategory === category) {
+                div.style.display = 'block'
+            } else {
+                div.style.display = 'none'
+            }
+        }
+    }
+}
+
+select.addEventListener('change', function () {
+    const filterVal = select.value;
+    if (!filterVal) {
+        return;
+    }
+    filterAndDisplay(filterVal);
+})
