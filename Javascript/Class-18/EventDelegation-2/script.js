d6ced95bb036d2131f312006a841b1a6603084df
Sabeel Khan <sabeelhps@gmail.com>
2025-11-19 21:23:17 +0530

    event delegation

---
+++ Javascript/Class-18/EventDelegation-2/script.js
@@ -0,0 +1,9 @@
+/*
+
+    create a set of colored boxes, and clicking 
+    on any box will change its background color 
+    using event delegation.
+
+    if any box is clicked then you have to assign a random background color.
+
+*/
