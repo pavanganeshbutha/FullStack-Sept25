c46ea41e310f76573f04770dea39b4b50d86aaaa
Sabeel Khan <sabeelhps@gmail.com>
2025-11-21 22:41:58 +0530

    Debouncing

--- Javascript/Class-19/Debouncing/script.js
+++ Javascript/Class-19/Debouncing/script.js
@@ -2,27 +2,47 @@ const input = document.querySelector('input');
 
 let count = 1;
 
-function fetchApi(query) {
+function fetchApi() {
     console.log(`Fetching, count: ${count++}`);
 }
 // first timer , id=1 - gone
 // second timer, id=2 - gone
 // third timer, id=3
 
-function debounce() {
+function debounce(delay) {
     let id = null;
     return function () {
         clearTimeout(id);
         id = setTimeout(function () {
             fetchApi();
-        }, 500);
+        }, delay);
     }
 }
 
-const optimisedFetch = debounce();
+const optimisedFetch = debounce(500);
 // console.log(optimisesFetch);
+
+// 501 
+// prev = 501
+
+// curr = 1002
+
+// prev = 1002
+// curr = 1503
+function throttle() {
+    let prev = new Date().getTime();
+    return function () {
+        const curr = new Date().getTime();
+        if (curr - prev > 500) {
+            fetchApi();
+            prev = curr;
+        }
+    }
+}
+
+const throttleOptimisedFetch = throttle();
 
 input.addEventListener('keyup', function () {
     const query = input.value;
-    optimisedFetch(query);
+    throttleOptimisedFetch(query);
 });
