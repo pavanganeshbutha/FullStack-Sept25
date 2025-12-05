286ee67f23a9c13574b745bd2ae2e9b5dd887c31
Sabeel Khan <sabeelhps@gmail.com>
2025-12-05 23:28:30 +0530

    Promise combinators and their polyfills

--- Javascript/Class-25/Polyfills/script.js
+++ Javascript/Class-25/Polyfills/script.js
@@ -3,6 +3,10 @@ const p1 = Promise.resolve(100);
 const p2 = Promise.resolve(200);
 const p3 = Promise.resolve(300);
 
+const p4 = new Promise((resolve) => setTimeout(() => resolve(400), 400));
+const p5 = new Promise((resolve, reject) => setTimeout(() => resolve(500), 200));
+const p6 = new Promise((resolve) => setTimeout(() => resolve(600), 300));
+
 /*
 [p1, p2, p3]
 
@@ -35,11 +39,36 @@ Promise.myAll = function (iterable) {
     });
 }
 
-Promise.myAll([1, 2, 3, 4, p1, p2, p3])
-    .then((res) => console.log(res))
-    .catch((err) => console.log('myall : Inside catch - ' + err));
+
+Promise.myRace = function (arr) {
+    return new Promise((resolve, reject) => {
+        arr.forEach((promise) => {
+            Promise.resolve(promise)
+                .then((val) => {
+                    resolve(val);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
+        })
+    })
+}
 
 
-Promise.all([1, 2, 3, 4, p1, p2, p3])
-    .then((res) => console.log(res))
-    .catch((err) => console.log('all: catch - ' + err));
+
+// Promise.myAll([1, 2, 3, 4, p1, p2, p3])
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log('myall : Inside catch - ' + err));
+
+
+// Promise.all([1, 2, 3, 4, p1, p2, p3])
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log('all: catch - ' + err));
+
+Promise.race([p4, p5, p6])
+ .then((res) => console.log(res))
+    .catch((err) => console.log('race: catch - ' + err));
+ 
+Promise.myRace([p4, p5, p6])
+ .then((res) => console.log(res))
+ .catch((err) => console.log('myRace: catch - ' + err));
